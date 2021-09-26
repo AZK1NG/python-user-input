bdbe68d67a2000b64dee8ff64d4d0e50e5324676
Azazal King <king@Azazals-MacBook-Pro.local>
2021-09-26 16:13:07 -0500

    add py program

---
+++ pythonpractice.py
@@ -0,0 +1,78 @@
+
+
+
+### # ## #
+# hours = input("enter hours worked ")
+# rate = input("enter pay rate ")
+# total = float(hours) * float(rate)
+
+# print (total)
+### # ## #
+
+
+# # # #
+# Name = input("enter your name ")
+# Date = input("enter the date ")
+# favfood = input("enter your favorite food ")
+ 
+# print("  ")
+# print("Name: " + Name)
+# print("  ")
+# print("Date: " + Date)
+# print("  ")
+# print("Food: " + favfood)
+# print("  ")
+# # # #
+
+
+## ##
+# x = 5
+# print(x + 10)
+## ##
+
+
+## # ##
+# x = 10
+# print(x + 10)
+## # ##
+
+
+#####Power of **
+
+# x = 10 
+# print (x ** 2)
+
+#####remainder %
+
+
+#hours 35 rate 2.75 pay 96.25
+
+
+hours = input("enter your hours worked: ")
+rate = input("enter your rate: ")
+
+
+pay = float(rate) * float(hours)
+print("your gross income is: ", pay)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
